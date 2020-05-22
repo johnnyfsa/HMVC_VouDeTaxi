577dc7d09d3bbf3b9d6b214892f9684bd46713f1
joi <joimarbgf@gmail.com>
2020-05-22 15:10:00 -0300

    Identação e sem os file controllers

--- src/control/Copia_Cliente_controller.java
+++ src/control/Copia_Cliente_controller.java
@@ -19,52 +19,56 @@ public class Copia_Cliente_controller extends Copia_Usuario_Controller
 {
 	private ArrayList<Pessoa_Fisica> L = new ArrayList<Pessoa_Fisica>();
 	
-//	private static void extracted() 
-//	{
-//		
-//	}
 	
 	public void jsonRead() 
 	{
 			
-		   	JSONObject jsonObject;
-	        JSONArray jsonArray;
-	        //Cria o parse de tratamento
-	        JSONParser parser = new JSONParser();
-	        //Variaveis que irao armazenar os dados do arquivo JSON
-	        try {
-	        	FileReader file = new FileReader("clientes.json");
-	        	if(file.read() ==-1) 
-	        	{
-	        		System.out.println("Arquivo vazio\n");
+		JSONObject jsonObject;
+	    JSONArray jsonArray;
+	    //Cria o parse de tratamento
+	    JSONParser parser = new JSONParser();
+	    //Variaveis que irao armazenar os dados do arquivo JSON
+	     try 
+	     {
+	        FileReader file = new FileReader("clientes.json");
+	        if(file.read() ==-1) 
+	        {
+	        	System.out.println("Arquivo vazio\n");
 	        		//extracted();
-	        	}
-	        	else
-	        	{
-	        		//o java n�o aceita resetar a filereader pra posi��o inicial, ent�o cria outro filereader
-	        		file = new FileReader("clientes.json");
-	        	}
-	        	jsonArray = (JSONArray) parser.parse(file);
-	        	Iterator<JSONObject> externalIterator = jsonArray.iterator();
+	        }
+	        else
+	        {
+	        	//o java n�o aceita resetar a filereader pra posi��o inicial, ent�o cria outro filereader
+	        	file = new FileReader("clientes.json");
+	        }
+	        	
+	        jsonArray = (JSONArray) parser.parse(file);
+	        Iterator<JSONObject> externalIterator = jsonArray.iterator();
 	  
-	            while(externalIterator.hasNext()) 
-	            {
-	            	Pessoa_Fisica aux = new Pessoa_Fisica();
-	            	jsonObject = externalIterator.next();
-	            	 aux.setNome((String) jsonObject.get("Nome"));
-	                 aux.setUsuario((String) jsonObject.get("Usuario"));
-	                 aux.setSenha((String) jsonObject.get("Senha"));
-	                 aux.setCpf((String) jsonObject.get("CPF"));
+	        while(externalIterator.hasNext()) 
+	        {
+	            Pessoa_Fisica aux = new Pessoa_Fisica();
+	            jsonObject = externalIterator.next();
+	            aux.setNome((String) jsonObject.get("Nome"));
+	            aux.setUsuario((String) jsonObject.get("Usuario"));
+	            aux.setSenha((String) jsonObject.get("Senha"));
+	            aux.setCpf((String) jsonObject.get("CPF"));
 	                
-	                 L.add(aux);
-	            }
+	            L.add(aux);
+	          }
+	       } 
+	        
+	     //Trata as exceptions que podem ser lan�adas no decorrer do processo
+	       catch (FileNotFoundException e) 
+	     	{
+	            e.printStackTrace();
 	        } 
-	        //Trata as exceptions que podem ser lan�adas no decorrer do processo
-	        catch (FileNotFoundException e) {
+	     	catch (IOException e) 
+	     	{
 	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } catch (ParseException e) {
+	        }
+	     	catch (ParseException e) 
+	     	{
 	            // TODO Auto-generated catch block
 	            e.printStackTrace();
 	        } 
@@ -73,39 +77,33 @@ public class Copia_Cliente_controller extends Copia_Usuario_Controller
 	public void jsonWrite() 
 	{
 		//Cria um Objeto JSON
-        JSONArray arrJson = new JSONArray();
-        
+        JSONArray arrJson = new JSONArray(); 
         FileWriter writeFile = null;
-        
-
         Iterator<Pessoa_Fisica> clienteIterator = L.iterator();
-
-       
-        
-        
-        
 
         while(clienteIterator.hasNext()) 
         {
         	JSONObject jsonObject = new JSONObject();
         	Pessoa_Fisica aux = new Pessoa_Fisica();
         	aux = clienteIterator.next();
-        	 //Armazena dados em um Objeto JSON
+        	
+        	//Armazena dados em um Objeto JSON
         	jsonObject.put("Usuario", aux.getUsuario());
             jsonObject.put("Nome", aux.getNome());
             jsonObject.put("Senha", aux.getSenha());
             jsonObject.put("CPF", aux.getCpf());
-           
-           arrJson.add(jsonObject);
+            arrJson.add(jsonObject);
         }
          
-        try{
+        try
+        {
             writeFile = new FileWriter("clientes.json");
             //Escreve no arquivo conteudo do Objeto JSON
             writeFile.write(arrJson.toJSONString());
             writeFile.close();
         }
-        catch(IOException e){
+        catch(IOException e)
+        {
             e.printStackTrace();
         }
 	}
@@ -113,30 +111,23 @@ public class Copia_Cliente_controller extends Copia_Usuario_Controller
 	public void adicionar(String nome, String usuario, String senha, String cpf)
 	{
 		 
-		jsonRead(); 
-		 
-		 
-		 	Pessoa_Fisica cliente = new Pessoa_Fisica();
+		jsonRead(); 		 
+		Pessoa_Fisica cliente = new Pessoa_Fisica();
 		
-	     
+		cliente.setNome(nome);
+	    cliente.setCpf(cpf);
+	    cliente.setUsuario(usuario);
+	    cliente.setSenha(senha);
+	    L.add(cliente);
 	        
-	        
-	        cliente.setNome(nome);
-	        cliente.setCpf(cpf);
-	        cliente.setUsuario(usuario);
-	        cliente.setSenha(senha);
-	        L.add(cliente);
-	        
-	      jsonWrite();
+	    jsonWrite();
            	
 	}
 	
 	public void remover(String nome, String usuario, String cpf)
 	{
 		ArrayList<Pessoa_Fisica> lista = busca(nome, usuario, cpf);
-		
-			
-		
+				
 		// Prepara a união
 		ArrayList<Pessoa_Fisica> union = new ArrayList<Pessoa_Fisica>(lista);
 		union.addAll(L);
@@ -148,9 +139,7 @@ public class Copia_Cliente_controller extends Copia_Usuario_Controller
 		// Atualiza a lista
 		L = union;
 		
-		jsonWrite();
-		
-		
+		jsonWrite();		
 		
 	}
 	
